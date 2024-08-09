81437e8cbce5b74a6b3039d2bc5fdb8841630492
fflinkexin <fflinkexin@chinatelecom.cn>
2024-08-09 16:39:07 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,7 @@
+["exit", "logger", "spinner", "env"].forEach((m) => {
+  Object.assign(exports, require(`./lib/${m}`));
+});
+
+exports.chalk = require("chalk");
+exports.execa = require("execa");
+exports.semver = require("semver");
